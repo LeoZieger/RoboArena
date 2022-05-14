3a8574f5dd873bdb7c68f77d153d2cbf8bf44d9f
lasse <lassefniederkrome@gmail.com>
2022-05-14 16:20:23 +0200

    Flake8 Test passed

--- code/BasicRobot.py
+++ code/BasicRobot.py
@@ -2,8 +2,6 @@
 
 
 # This is important for drawing the robot later
-from PyQt5.QtGui import QPainter, QBrush, QPen
-from PyQt5.QtCore import Qt
 import TestDrawing
 
 
@@ -16,8 +14,6 @@ class BasicRobot:
         self.r = r                          # radius
         self.alpha = alpha                  # direction
 
-
-
     # Small function that shows all robot-info.
     def info(self):
         print(self.x)
@@ -25,18 +21,10 @@ class BasicRobot:
         print(self.r)
         print(self.alpha)
 
+#  def spawnRobot(self, x,y,r,alpha):
 
 
-
-   # def spawnRobot(self, x,y,r,alpha):
-
-
-
-
-
-
-
-testRobot = BasicRobot(0,0,50,0)
+testRobot = BasicRobot(0, 0, 50, 0)
 
 testRobot.info()
 
