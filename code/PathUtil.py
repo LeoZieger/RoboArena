a7f7f9d6a238f0bbaa377c8c1c46cae8b41db3cc
Lukas Reutemann <70217976+Luke-537@users.noreply.github.com>
2022-07-22 17:21:38 +0200

    Hotfixed flake8 checks

--- code/PathUtil.py
+++ code/PathUtil.py
@@ -1,7 +1,9 @@
 import pathlib
 
+
 def getDir(dir):
     return (pathlib.Path(__file__).parent / dir).__str__()
+
 
 def getPath(dir, file):
     return (pathlib.Path(__file__).parent / dir / file).__str__()
