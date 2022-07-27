9ef61be000b4d40378fdbe13538bbc74c2e744e1
an-trieu <104647714+an-trieu@users.noreply.github.com>
2022-07-27 23:29:59 +0200

    Update NameInput.py
    Hotfix

--- code/NameInput.py
+++ code/NameInput.py
@@ -1,9 +1,9 @@
 from PyQt5 import QtWidgets
 from PyQt5.QtGui import QIcon
 from PathUtil import getPath
+from PyQt5.QtWidgets import QDesktopWidget
 
 
-# Popup where you can insert the name of a map
 class NameInput(QtWidgets.QInputDialog):
     def __init__(self):
         super().__init__()
@@ -14,4 +14,11 @@ class NameInput(QtWidgets.QInputDialog):
         self.setLabelText("Please enter the map-name below!")
         self.setWindowTitle("Save File")
         self.setWindowIcon(QIcon(getPath("res", "blue_tank.png")))
+        self.centerWindowOnScreen()
         self.show()
+
+    def centerWindowOnScreen(self):
+        outerRect = self.frameGeometry()
+        centerOfScreen = QDesktopWidget().availableGeometry().center()
+        outerRect.moveCenter(centerOfScreen)
+        self.move(outerRect.topLeft())
